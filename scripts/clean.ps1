daeeadc7aaca4b2772a44a7b5f46e75d7aeb623a
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2022-04-27 13:03:50 -0700

    Initial commit

---
+++ scripts/clean.ps1
@@ -0,0 +1,2 @@
+deactivate
+rm -Recurse env/
---
+++ scripts/setup.ps1
@@ -0,0 +1,78 @@
+python -m venv env
+./env/Scripts/activate
+python -m pip install --upgrade pip
+
+New-Item -Path . -Name 'requirements.txt' -ItemType 'file' -Value @"
+pygame
+typing
+"@
+New-Item -Path . -Name 'requirements-dev.txt' -ItemType 'file' -Value @"
+pytest
+pygame
+typing
+"@
+python -m pip install -r 'requirements-dev.txt'
+
+New-Item -Path . -Name 'main.py' -ItemType 'file' -Value @"
+#################################
+# main.py
+#-------------------------------
+# J Karstin Neill    MM.DD.YYYY
+#################################
+
+
+### IMPORTS ###
+
+import pygame
+import sys
+import typing
+
+
+### CONSTANTS ###
+
+EXIT_SUCCESS = 0
+
+
+### MAIN FUNCTION DEFINITION ###
+
+def main(argv: list[str], argc: int) -> int:
+    print('Hello world!')
+    
+    if argc > 1:
+        print(argv)
+
+    return EXIT_SUCCESS
+
+
+### CORE FUNCTIONS ###
+
+
+
+
+### UTILITY FUNCTIONS ###
+
+
+
+
+### MAIN FUNCTION EXECUTION ###
+
+if __name__ == '__main__':
+    pygame.init()
+    argv: list[str] = sys.argv
+    argc: int = len(argv)
+    sys.exit(main(argv, argc))
+
+"@
+
+$repo = Split-Path -Path $pwd -Leaf
+New-Item -Path . -Name '.gitignore' -ItemType 'file' -Value @"
+env/
+*.ps1
+"@
+New-Item -Path . -Name 'README.md' -ItemType 'file' -Value @"
+# $repo
+"@
+
+git init .
+git add .
+git commit -m "Initial commit"
