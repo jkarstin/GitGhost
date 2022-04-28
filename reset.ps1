a6800b8218b7e2e9ccbe8a79a9606632d8d4502c
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2022-04-28 13:33:36 -0700

    Update naming conventions in reset

--- reset.ps1
+++ reset.ps1
@@ -2,16 +2,18 @@ if ($args.count -ne 1) {
     Write-Error "Requires single argument <path_to_target_repo>!"
 }
 else {
-    $path = $args[0]
+    $buildrepo = $args[0]
     if (-Not (Test-Path $path)) {
-        Write-Error "Given <path_to_target_repo> ['$path'] could not be located!"
+        Write-Error "Given <path_to_target_repo> ['$buildrepo'] could not be located!"
     }
     else {
-        $old = Split-Path $MyInvocation.MyCommand.Path -Parent
-        Set-Location $old
+        $ggold = Split-Path $MyInvocation.MyCommand.Path -Parent
+
+        Set-Location $ggold
         git remote rm origin
-        Set-Location $path
+
+        Set-Location $buildrepo
         git clone https://github.com/jkarstin/GitGhost
-        Remove-Item -Recurse -Force $old
+        Remove-Item -Recurse -Force $ggold
     }
 }
