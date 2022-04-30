f6f1421e6d04a87d7b8df5decfefd9846e2d821c
jkarstin <karstinneill@gmail.com>
2022-04-29 22:25:35 -0700

    Added deepcleen to GG

---
+++ ghost.carton/GG.egg/.yolk/GG/deepclean.ps1
@@ -0,0 +1,16 @@
+$GG = Split-Path $MyInvocation.MyCommand.Path -Parent
+
+if (Split-Path $GG -Leaf -ne "GG") {
+    Write-Error "Unsafe to execute this script unless it is within the GG directory!"
+    Exit 86
+}
+
+$Kitchen = Resolve-Path "$GG\.."
+
+if (Test-Path "$Kitchen\.git") {
+    Set-Location $Kitchen
+    git remote rm origin
+}
+
+Set-Location "$Kitchen\.."
+Remove-Item -Recurse -Force $Kitchen
