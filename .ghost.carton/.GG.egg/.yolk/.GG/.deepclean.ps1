096c356af82c159ac03e743455fe8af71a9a3dd6
jkarstin <karstinneill@gmail.com>
2022-05-02 14:18:49 -0700

    Fixed .GG.deepclean script bug

--- .ghost.carton/.GG.egg/.yolk/.GG/.deepclean.ps1
+++ .ghost.carton/.GG.egg/.yolk/.GG/.deepclean.ps1
@@ -1,7 +1,7 @@
 $GG = Split-Path $MyInvocation.MyCommand.Path -Parent
 
-if ((Split-Path $GG -Leaf) -ne "GG") {
-    Write-Error "Unsafe to execute this script unless it is within the GG directory!"
+if ((Split-Path $GG -Leaf) -ne ".GG") {
+    Write-Error "Unsafe to execute this script unless it is within the .GG directory!"
     Exit 86
 }
 
